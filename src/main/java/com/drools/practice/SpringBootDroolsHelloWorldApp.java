84c178e803226710c07bf819ab79038a0eaa0f6d
Anil Chaurshiya <anil.chaurshiya@gmail.com>
2017-09-04 22:27:27 -0500

    spring boot application using drools decision table

---
+++ src/main/java/com/drools/practice/SpringBootDroolsHelloWorldApp.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.drools.practice;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author anilc
+ *
+ */
+@SpringBootApplication
+public class SpringBootDroolsHelloWorldApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDroolsHelloWorldApp.class, args);
+
+	}
+
+	@Bean
+	public KieContainer kieContainer() {
+		return KieServices.Factory.get().getKieClasspathContainer();
+	}
+
+}
